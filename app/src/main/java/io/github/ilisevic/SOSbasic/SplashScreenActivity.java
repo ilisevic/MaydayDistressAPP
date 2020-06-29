27ba49a426a6b5cd69e8067c96218f113b6eae51
mladen <ilisevic@gmail.com>
2020-06-29 16:02:45 +0200

    Dorađeno trajanje splash screena potrebno još dodati onClick gašenje ove i paljenje MainActivity

--- app/src/main/java/io/github/ilisevic/SOSbasic/SplashScreenActivity.java
+++ app/src/main/java/io/github/ilisevic/SOSbasic/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package io.github.ilisevic.SOSbasic;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.annotation.FontRes;
@@ -14,16 +15,16 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+        final EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
                 .withFullScreen()
                 .withTargetActivity(MainActivity.class)
-                .withSplashTimeOut(3000)
+                .withSplashTimeOut(2500)
                 .withBackgroundColor(Color.parseColor("#DF2520"))
                 // .withHeaderText("To be sure that compass shows right direction please calibrate the phone's sensor magnetometer by holding phone up and moving it in a figure of number 8.")
-                // .withFooterText("\"To be sure that compass shows right direction please calibrate the phone's sensor magnetometer by holding phone up and moving it in a figure of number 8.\"")
+                .withFooterText("\n SOS Beacon & Locator 2020")
                 // .withBeforeLogoText("To be sure that compass shows right direction please calibrate the phone's sensor magnetometer by holding phone up and moving it in a figure of number 8.")
-                // .withAfterLogoText("\"To be sure that compass shows right direction please calibrate the phone's sensor magnetometer by holding phone up and moving it in a figure of number 8.\"")
-                .withBackgroundResource(R.drawable.splashlogo);
+                // .withAfterLogoText("\"Tap to close\"")
+                .withBackgroundResource(R.drawable.splash3);
         // .withLogo(R.mipmap.ic_splash_screen_foreground);//TODO promjeniti  drugim logom
 
         // Typeface atwriter = Typeface.setTypeface(ResourcesCompat.getFont(context, R.font.abc_font))
@@ -32,14 +33,26 @@ public class SplashScreenActivity extends AppCompatActivity {
 
 //        config.getHeaderTextView().setTextColor(Color.WHITE);
 //        config.getHeaderTextView().setTextSize(2);
-//        config.getFooterTextView().setTextColor(Color.WHITE);
+        config.getFooterTextView().setTextColor(Color.LTGRAY);
 //        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
 //        config.getBeforeLogoTextView().setTextSize(2);
-
+        config.getFooterTextView().setTextSize(12);
         //config.getAfterLogoTextView().setTextColor(Color.WHITE);
         //    config.getAfterLogoTextView().setTypeface(atwriter);
 
-        View easySplashScreen = config.create();
+        final View easySplashScreen = config.create();
         setContentView(easySplashScreen);
+
+//     easySplashScreen.setOnClickListener(new View.OnClickListener() {
+//         @Override//TODO uraditi gašenje na click
+//         public void onClick(View v) {
+//            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+//            // config.withSplashTimeOut(0);
+//             setContentView(easySplashScreen);
+//             startActivity(i);
+//           //  finish();
+//
+//         }
+//     });
     }
 }
